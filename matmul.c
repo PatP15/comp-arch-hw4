6f36d060cbb26ce9d208e9450319dae297c7675f
Pat <DN@SUGANDEEZ.com>
2023-12-08 07:09:12 +0000

    Now print the cycles

--- matmul.c
+++ matmul.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <immintrin.h>
+#include <stdint.h>
 
 #define SIZE 10
 
-void matrixMultiplication(double A[SIZE][SIZE], double B[SIZE][SIZE], double C[SIZE][SIZE]) {
+void serialMatrixMult(double A[SIZE][SIZE], double B[SIZE][SIZE], double C[SIZE][SIZE]) {
     for (int i = 0; i < SIZE; ++i) {
         for (int j = 0; j < SIZE; ++j) {
             C[i][j] = 0;
@@ -12,6 +14,37 @@ void matrixMultiplication(double A[SIZE][SIZE], double B[SIZE][SIZE], double C[S
             }
         }
     }
+}
+
+void vecMatrixMult(double A[SIZE][SIZE], double B[SIZE][SIZE], double C[SIZE][SIZE]) {
+    for (int i = 0; i < SIZE; i++) {
+        for (int j = 0; j < SIZE; j++) {
+            __m256d sum = _mm256_setzero_pd(); // Initialize sum vector to zero
+
+            for (int k = 0; k < SIZE; k += 4) {
+                // Load 4 double-precision elements from A and B
+                __m256d a = _mm256_loadu_pd(&A[i][k]);
+                __m256d b = _mm256_loadu_pd(&B[k][j]);
+
+                // Perform element-wise multiplication and add to sum
+                sum = _mm256_add_pd(sum, _mm256_mul_pd(a, b));
+            }
+
+            // Horizontally add the elements of the sum vector and store the result in C[i][j]
+            sum = _mm256_hadd_pd(sum, sum);
+            __m128d sum_high = _mm256_extractf128_pd(sum, 1);
+            __m128d result = _mm_add_pd(_mm256_castpd256_pd128(sum), sum_high);
+            C[i][j] = _mm_cvtsd_f64(result);
+        }
+    }
+}
+
+static inline uint64_t rdtsc(void) {
+
+    unsigned int lo, hi;
+    asm volatile("mfence");
+    asm volatile("rdtsc": "=a" (lo), "=d" (hi));
+    return ((uint64_t)hi << 32) | lo;
 }
 
 int main() {
@@ -47,8 +80,16 @@ int main() {
     printf("\n");
 
     // Perform matrix multiplication
-    matrixMultiplication(A, B, C);
+    serialMatrixMult(A, B, C);
 
+    for (int i = 0; i < SIZE; ++i) {
+        for (int j = 0; j < SIZE; ++j) {
+            printf("%lf ", C[i][j]);
+        }
+        printf("\n\n");
+    }
+
+    vecMatrixMult(A, B, C);
     for (int i = 0; i < SIZE; ++i) {
         for (int j = 0; j < SIZE; ++j) {
             printf("%lf ", C[i][j]);
--- softmax.c
+++ softmax.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <math.h>
 #include <immintrin.h>
-
+#include <stdint.h>
 #define VECTOR_LENGTH 10
 
 void softmax(double *x, int size) {
@@ -15,11 +15,11 @@ void softmax(double *x, int size) {
     double sum = 0.0;
     for (int i = 0; i < size; ++i) {
         x[i] = exp(x[i] - max_x);
-        printf("%lf", x[i]);
+        //printf("%lf", x[i]);
 	sum += x[i];
     }
-    printf("\n");
-    printf("%lf\n", sum);
+    //printf("\n");
+    //printf("%lf\n", sum);
 
     for (int i = 0; i < size; ++i) {
         x[i] /= sum;
@@ -76,7 +76,7 @@ void softmax_vectorized(double *x, int size) {
 	x[i] = exp(x[i]-max_x);
 	remaining += x[i];
     }
-    printf("remaining sum: %lf\n", remaining);
+    //printf("remaining sum: %lf\n", remaining);
     //get final sum
     double avx_result[4];
     _mm256_store_pd(avx_result, sum);
@@ -86,7 +86,7 @@ void softmax_vectorized(double *x, int size) {
     }
     sumFinal += remaining;
 
-    printf("sum final: %lf\n", sumFinal);
+    //printf("sum final: %lf\n", sumFinal);
 
     __m256d invSum = _mm256_set1_pd(1.0/sumFinal);
 
@@ -103,7 +103,61 @@ void softmax_vectorized(double *x, int size) {
     }
 
 }
+
+static inline uint64_t rdtsc(void) {
+
+    unsigned int lo, hi;
+    asm volatile("mfence");
+    asm volatile("rdtsc": "=a" (lo), "=d" (hi));
+    return ((uint64_t)hi << 32) | lo;
+}
+
+void randVector(double* x)
+{
+     // Initialize vector x with random values
+    for (int i = 0; i < VECTOR_LENGTH; ++i) {
+        x[i] = (double)rand() / RAND_MAX;
+    }
+}
+
 int main() {
+    
+    double *x = malloc(VECTOR_LENGTH * sizeof(double));
+    double *y = malloc(VECTOR_LENGTH * sizeof(double));
+
+    randVector(x);
+    for (int i = 0; i < VECTOR_LENGTH; ++i) {
+        printf("%lf ", x[i]);
+    }
+    printf("\n");
+
+    
+    int repeats = 5;
+    // Serial softmax computation
+
+    for(int i = 0; i < repeats; i++)
+    {
+        randVector(x);
+        uint64_t start = rdtsc();
+        softmax(x, VECTOR_LENGTH);
+        uint64_t end = rdtsc();
+        printf("Serial time taken: %lu cycles\n", end - start);
+    }
+   
+    // Vector softmax computation
+   
+    for(int i = 0; i < repeats; i++)
+    {
+        randVector(y);
+        uint64_t start = rdtsc();
+        softmax_vectorized(y, VECTOR_LENGTH);
+        uint64_t end = rdtsc();
+        printf("Vector time taken: %lu cycles\n", end - start);
+    }
+   
+    
+    
+   /*
     double *x = malloc(VECTOR_LENGTH * sizeof(double));
     double *y = malloc(VECTOR_LENGTH * sizeof(double));
 
@@ -133,6 +187,8 @@ int main() {
     printf("\n");
 
     free(x);
-    free(y);
+    free(y);*/
+    free(x);
+    //free(y);
     return 0;
 }
